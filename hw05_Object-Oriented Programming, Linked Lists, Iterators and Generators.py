35d9345733c9a4c5f6c8d02bc9546d9767208a60
sharon-chx <69126372+sharon-chx@users.noreply.github.com>
2021-08-08 11:13:16 -0400

    Add files via upload
    Oriented Programming, Linked Lists, Iterators and Generators

---
+++ hw05_Object-Oriented Programming, Linked Lists, Iterators and Generators.py
@@ -0,0 +1,251 @@
+
+passphrase = '*** PASSPHRASE HERE ***'
+
+
+def survey(p):
+    """
+    You do not need to understand this code.
+    >>> survey(passphrase)
+    '387ca4740bd8e6178c7e1d7742a3491fb566f2f21815445de0e8c4ba'
+    """
+    import hashlib
+    return hashlib.sha224(p.encode('utf-8')).hexdigest()
+
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is empty. Please restock.'
+    >>> v.add_funds(15)
+    'Machine is empty. Please restock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must add $10 more funds.'
+    >>> v.add_funds(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must add $3 more funds.'
+    >>> v.add_funds(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.add_funds(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.add_funds(15)
+    'Machine is empty. Please restock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.add_funds(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    "*** YOUR CODE HERE ***"
+    def __init__(self, merch, price, stock=0, balance=0):
+        self.merch = merch
+        self.price = price
+        self.stock = stock
+        self.balance = balance
+    
+    def restock(self, stock):
+        self.stock += stock
+        return f"Current {self.merch} stock: {self.stock}"
+    
+    def vend(self):
+        change = self.balance - self.price
+        if self.stock == 0:
+            return f"Machine is empty. Please restock."
+        elif change < 0:
+            return f"You must add ${-change} more funds."
+        elif change == 0:
+        	self.balance = 0
+        	self.stock -= 1
+        	return f"Here is your {self.merch}."
+        else:
+        	self.balance = 0
+        	self.stock -= 1
+        	return f"Here is your {self.merch} and ${change} change."
+             
+    def add_funds(self, amount):
+        if self.stock == 0:
+            return f"Machine is empty. Please restock. Here is your ${amount}."
+        else:
+            self.balance += amount
+            return f"Current balance: ${self.balance}"
+
+
+def store_digits(n):
+    """Stores the digits of a positive number n in a linked list.
+
+    >>> s = store_digits(1)
+    >>> s
+    Link(1)
+    >>> store_digits(2345)
+    Link(2, Link(3, Link(4, Link(5))))
+    >>> store_digits(876)
+    Link(8, Link(7, Link(6)))
+    >>> # a check for restricted functions
+    >>> import inspect, re
+    >>> cleaned = re.sub(r"#.*\\n", '', re.sub(r'"{3}[\s\S]*?"{3}', '', inspect.getsource(store_digits)))
+    >>> print("Do not use str or reversed!") if any([r in cleaned for r in ["str", "reversed"]]) else None
+    """
+    "*** YOUR CODE HERE ***"
+    result = Link.empty
+    while n>0:
+        result = Link(n%10, result)
+        n = n//10
+    return result
+    
+
+
+
+def path_yielder(t, value):
+    """Yields all possible paths from the root of t to a node with the label value
+    as a list.
+
+    >>> t1 = Tree(1, [Tree(2, [Tree(3), Tree(4, [Tree(6)]), Tree(5)]), Tree(5)])
+    >>> print(t1)
+    1
+      2
+        3
+        4
+          6
+        5
+      5
+    >>> next(path_yielder(t1, 6))
+    [1, 2, 4, 6]
+    >>> path_to_5 = path_yielder(t1, 5)
+    >>> sorted(list(path_to_5))
+    [[1, 2, 5], [1, 5]]
+
+    >>> t2 = Tree(0, [Tree(2, [t1])])
+    >>> print(t2)
+    0
+      2
+        1
+          2
+            3
+            4
+              6
+            5
+          5
+    >>> path_to_2 = path_yielder(t2, 2)
+    >>> sorted(list(path_to_2))
+    [[0, 2], [0, 2, 1, 2]]
+    """
+
+    "*** YOUR CODE HERE ***"
+    if t.label == value:
+    	yield [t.label]
+    for branch in t.branches:
+    	for path in path_yielder(branch, value):
+    		yield [t.label] + path
+
+
+def preorder(t):
+    """Return a list of the entries in this tree in the order that they
+    would be visited by a preorder traversal (see problem description).
+
+    >>> numbers = Tree(1, [Tree(2), Tree(3, [Tree(4), Tree(5)]), Tree(6, [Tree(7)])])
+    >>> preorder(numbers)
+    [1, 2, 3, 4, 5, 6, 7]
+    >>> preorder(Tree(2, [Tree(4, [Tree(6)])]))
+    [2, 4, 6]
+    """
+    "*** YOUR CODE HERE ***"
+    lis = [t.label]
+    for branch in t.branches:
+        if not branch.is_leaf():
+            for bran in preorder(branch):   #since preorder(branch) returns a list, so need to use a foor loop to append lis with every element from preorder(branch)
+                lis.append(bran)
+        else:
+            lis.append(branch.label)
+    return lis
+
+
+
+class Link:
+    """A linked list.
+
+    >>> s = Link(1)
+    >>> s.first
+    1
+    >>> s.rest is Link.empty
+    True
+    >>> s = Link(2, Link(3, Link(4)))
+    >>> s.first = 5
+    >>> s.rest.first = 6
+    >>> s.rest.rest = Link.empty
+    >>> s                                    # Displays the contents of repr(s)
+    Link(5, Link(6))
+    >>> s.rest = Link(7, Link(Link(8, Link(9))))
+    >>> s
+    Link(5, Link(7, Link(Link(8, Link(9)))))
+    >>> print(s)                             # Prints str(s)
+    <5 7 <8 9>>
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is not Link.empty:
+            rest_repr = ', ' + repr(self.rest)
+        else:
+            rest_repr = ''
+        return 'Link(' + repr(self.first) + rest_repr + ')'
+
+    def __str__(self):
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+
+class Tree:
+    """
+    >>> t = Tree(3, [Tree(2, [Tree(5)]), Tree(4)])
+    >>> t.label
+    3
+    >>> t.branches[0].label
+    2
+    >>> t.branches[1].is_leaf()
+    True
+    """
+
+    def __init__(self, label, branches=[]):
+        for b in branches:
+            assert isinstance(b, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __repr__(self):
+        if self.branches:
+            branch_str = ', ' + repr(self.branches)
+        else:
+            branch_str = ''
+        return 'Tree({0}{1})'.format(self.label, branch_str)
+
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
